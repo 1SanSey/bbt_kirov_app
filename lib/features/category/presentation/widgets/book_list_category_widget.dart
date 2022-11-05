0e5e50b573a6a8d795529d86a6694ab727b112f2
Сергей Огарков <lifesaver43@yandex.ru>
2022-11-05 15:23:25 +0300

    Modificated Search field, added onChange

--- lib/features/category/presentation/widgets/book_list_category_widget.dart
+++ lib/features/category/presentation/widgets/book_list_category_widget.dart
@@ -7,27 +7,56 @@ import 'package:bbt_kirov_app/core/widgets/book_card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BooksCategoryWidget extends StatelessWidget {
+class BooksCategoryWidget extends StatefulWidget {
   const BooksCategoryWidget({super.key, required this.query});
   final String query;
 
   @override
+  State<BooksCategoryWidget> createState() => _BooksCategoryWidgetState();
+}
+
+class _BooksCategoryWidgetState extends State<BooksCategoryWidget> {
+  List<BookEntity> _categoryBooks = [];
+  List<BookEntity> _foundBooks = [];
+
+  @override
+  void initState() {
+    _foundBooks = _categoryBooks;
+    super.initState();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<BookEntity> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = _categoryBooks;
+    } else {
+      results = _categoryBooks
+          .where((book) =>
+              book.name.toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+
+      setState(() {
+        _foundBooks = results;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
 // Загрузка всех книг
-    if (query == 'all') {
+    if (widget.query == 'all') {
       BlocProvider.of<CategoryAllBooksBloc>(context)
-          .add(CategoryLoadBooksEvent(param: query));
+          .add(CategoryLoadBooksEvent(param: widget.query));
 
       return BlocBuilder<CategoryAllBooksBloc, CategoryBooksState>(
           builder: (context, state) {
-        List<BookEntity> categoryBooks = [];
-
         if (state is CategoryBooksLoading) {
           return loadingIndicator();
         }
         if (state is CategoryBooksLoaded) {
-          categoryBooks = state.books;
-          if (categoryBooks.isEmpty) {
+          _categoryBooks = state.books;
+          _foundBooks = _categoryBooks;
+          if (_categoryBooks.isEmpty) {
             return showErrorText('Books are not loaded');
           }
         } else if (state is CategoryBooksError) {
@@ -37,11 +66,12 @@ class BooksCategoryWidget extends StatelessWidget {
         }
 
         return CustomScrollView(slivers: [
-          const SliverPadding(
-            padding: EdgeInsets.only(top: 16, right: 8, left: 8),
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 16, right: 8, left: 8),
             sliver: SliverToBoxAdapter(
               child: TextField(
-                decoration: InputDecoration(
+                onChanged: (value) => _runFilter(value),
+                decoration: const InputDecoration(
                   filled: true,
                   fillColor: AppColors.greyColor,
                   hintText: 'Поиск',
@@ -60,9 +90,9 @@ class BooksCategoryWidget extends StatelessWidget {
             sliver: SliverGrid(
               delegate: SliverChildBuilderDelegate(
                 (context, index) {
-                  return BookCard(book: categoryBooks[index]);
+                  return BookCard(book: _foundBooks[index]);
                 },
-                childCount: categoryBooks.length,
+                childCount: _foundBooks.length,
               ),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
@@ -77,11 +107,11 @@ class BooksCategoryWidget extends StatelessWidget {
     }
 
     // Загрузка книг по наименованию
-    if (query == 'Бхагавад-гита' ||
-        query == 'Шримад Бхагаватам' ||
-        query == 'Чайтанья Чаритамрита') {
+    if (widget.query == 'Бхагавад-гита' ||
+        widget.query == 'Шримад Бхагаватам' ||
+        widget.query == 'Чайтанья Чаритамрита') {
       BlocProvider.of<CategoryBooksByNameBloc>(context)
-          .add(CategoryLoadBooksEvent(param: query));
+          .add(CategoryLoadBooksEvent(param: widget.query));
 
       return BlocBuilder<CategoryBooksByNameBloc, CategoryBooksState>(
           builder: (context, state) {
@@ -125,12 +155,12 @@ class BooksCategoryWidget extends StatelessWidget {
     }
 
     // Загрузка книг по размеру
-    if (query == 'small' ||
-        query == 'medium' ||
-        query == 'big' ||
-        query == 'maha big') {
+    if (widget.query == 'small' ||
+        widget.query == 'medium' ||
+        widget.query == 'big' ||
+        widget.query == 'maha big') {
       BlocProvider.of<CategoryBooksBySizeBloc>(context)
-          .add(CategoryLoadBooksEvent(param: query));
+          .add(CategoryLoadBooksEvent(param: widget.query));
 
       return BlocBuilder<CategoryBooksBySizeBloc, CategoryBooksState>(
           builder: (context, state) {
@@ -174,9 +204,9 @@ class BooksCategoryWidget extends StatelessWidget {
     }
 
     // Загрузка наборов книг
-    if (query == 'set') {
+    if (widget.query == 'set') {
       BlocProvider.of<CategoryBooksSetBloc>(context)
-          .add(CategoryLoadBooksEvent(param: query));
+          .add(CategoryLoadBooksEvent(param: widget.query));
 
       return BlocBuilder<CategoryBooksSetBloc, CategoryBooksState>(
           builder: (context, state) {
@@ -220,9 +250,9 @@ class BooksCategoryWidget extends StatelessWidget {
     }
 
     // Загрузка кулинарных книг
-    if (query == 'culinary') {
+    if (widget.query == 'culinary') {
       BlocProvider.of<CategoryCulinaryBooksBloc>(context)
-          .add(CategoryLoadBooksEvent(param: query));
+          .add(CategoryLoadBooksEvent(param: widget.query));
 
       return BlocBuilder<CategoryCulinaryBooksBloc, CategoryBooksState>(
           builder: (context, state) {
