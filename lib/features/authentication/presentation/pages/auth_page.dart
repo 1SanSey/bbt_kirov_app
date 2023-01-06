4c7b2bb44df90fa7a6c705bfaa6dbf355a5fbc56
Sergey Ogarkov <lifesaver43@yandex.ru>
2023-01-06 19:55:32 +0300

    modificated auth screen

--- lib/features/authentication/presentation/pages/auth_page.dart
+++ lib/features/authentication/presentation/pages/auth_page.dart
@@ -1,4 +1,5 @@
 import 'package:bbt_kirov_app/core/assets/app_const.dart';
+import 'package:bbt_kirov_app/core/themes/app_colors.dart';
 import 'package:bbt_kirov_app/features/authentication/presentation/auth_bloc/auth_bloc.dart';
 import 'package:bbt_kirov_app/features/home/presentation/pages/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +21,8 @@ class AuthPageState extends State<AuthPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter Login/Logout'),
+          title: const Text('BBT Kirov App'),
+          centerTitle: true,
         ),
         body: Center(
           child: SingleChildScrollView(
@@ -31,25 +33,19 @@ class AuthPageState extends State<AuthPage> {
                 SizedBox(
                   height: 200,
                   child: Image.asset(AppConstants.bbtLogo),
-                  /* Image.network(
-                      'http://blog.back4app.com/wp-content/uploads/2017/11/logo-b4a-1-768x175-1.png'), */
+                ),
+                const SizedBox(
+                  height: 16,
                 ),
                 const Center(
-                  child: Text('Flutter on Back4App',
+                  child: Text('Выполните авторизацию для входа',
                       style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
                 ),
                 const SizedBox(
                   height: 16,
                 ),
-                const Center(
-                  child:
-                      Text('User Login/Logout', style: TextStyle(fontSize: 16)),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextField(
+                /* TextField(
                   controller: controllerUsername,
                   enabled: !isLoggedIn,
                   keyboardType: TextInputType.text,
@@ -58,65 +54,137 @@ class AuthPageState extends State<AuthPage> {
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.black)),
-                      labelText: 'Username'),
+                      labelText: 'Логин'),
+                ), */
+                TextField(
+                  cursorColor: AppColors.primaryColorLight,
+                  controller: controllerUsername,
+                  enabled: !isLoggedIn,
+                  keyboardType: TextInputType.text,
+                  textCapitalization: TextCapitalization.none,
+                  autocorrect: false,
+                  textInputAction: TextInputAction.search,
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Логин',
+                    labelStyle:
+                        const TextStyle(color: AppColors.primaryColorLight),
+                    fillColor: AppColors.greyColor,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: AppColors.greyColor,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: AppColors.primaryColorLight,
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
                 ),
                 const SizedBox(
                   height: 8,
                 ),
                 TextField(
+                  cursorColor: AppColors.primaryColorLight,
                   controller: controllerPassword,
                   enabled: !isLoggedIn,
                   obscureText: true,
                   keyboardType: TextInputType.text,
                   textCapitalization: TextCapitalization.none,
                   autocorrect: false,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      labelText: 'Password'),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                  height: 50,
-                  child: TextButton(
-                    onPressed: isLoggedIn ? null : () => doUserLogin(context),
-                    child: const Text('Login'),
+                  textInputAction: TextInputAction.search,
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Пароль',
+                    labelStyle:
+                        const TextStyle(color: AppColors.primaryColorLight),
+                    fillColor: AppColors.greyColor,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: AppColors.greyColor,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: AppColors.primaryColorLight,
+                        width: 1.0,
+                      ),
+                    ),
                   ),
                 ),
                 const SizedBox(
                   height: 16,
                 ),
                 SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: isLoggedIn
+                        ? null
+                        : () => doUserLogin(context) /* doUserLogin(context) */,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        fixedSize: const Size(320, 50),
+                        textStyle:
+                            const TextStyle(color: Colors.white, fontSize: 18),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8))),
+                    child: const Text('ВОЙТИ'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                /* SizedBox(
                   height: 50,
                   child: TextButton(
                     onPressed: !isLoggedIn ? null : () => doUserLogout(context),
                     child: const Text('Logout'),
                   ),
-                )
+                ) */
               ],
             ),
           ),
         ));
   }
 
-  void showSuccess(String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Success!"),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              child: const Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
+  Widget showSuccess() {
+    final login = controllerUsername.text.trim();
+    final password = controllerPassword.text.trim();
+    context
+        .read<AuthBLoC>()
+        .add(AuthEvent.logIn(login: login, password: password));
+
+    return BlocBuilder<AuthBLoC, AuthState>(
+      builder: (context, state) {
+        /* if (state.user.isAuthenticated) { */
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Успешно!"),
+              content: const Text('Вы успешно авторизовались.'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text("OK"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
         );
+        /* } */
+
+        return const SizedBox.shrink();
       },
     );
   }
@@ -141,22 +209,81 @@ class AuthPageState extends State<AuthPage> {
     );
   }
 
-  void doUserLogin(BuildContext context) async {
+  void doUserLogin(BuildContext context) {
     final login = controllerUsername.text.trim();
     final password = controllerPassword.text.trim();
     context
         .read<AuthBLoC>()
         .add(AuthEvent.logIn(login: login, password: password));
 
-    setState(() {
-      isLoggedIn = true;
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const HomePage(),
-        ),
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Успешно!"),
+          content: const Text('Вы успешно авторизовались.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    /* return BlocListener<AuthBLoC, AuthState>(listener: (context, state) {
+      state.maybeWhen(
+        orElse: () {
+          log('orElse');
+        },
+        authenticated: (state) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Успешно!"),
+                content: const Text('Вы успешно авторизовались.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text("OK"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        notAuthenticated: (state) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Error!"),
+                content: const Text('Произошла ошибка'),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text("OK"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
       );
-    });
+    }); */
   }
 
   void doUserLogout(BuildContext context) async {
